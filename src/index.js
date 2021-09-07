1fb997ac983c3aae9fcd11d70fc37534a4416d66
Ilchenko Yaroslav <73880344+IlchenkoY@users.noreply.github.com>
2021-09-08 01:21:26 +0300

    add toggle logic

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import './js/themeToggle.js';
+import './js/menuMaker.js';
---
+++ src/js/themeToggle.js
@@ -0,0 +1,35 @@
+const themeToggleEl = document.getElementById('theme-switch-toggle');
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+themeToggleEl.addEventListener('click', toggleClickHandler);
+
+
+currentThemeState();
+
+function toggleClickHandler() {
+    let currentTheme = document.body.classList.value;
+    console.log(currentTheme);
+
+    if (document.body.classList.contains(Theme.LIGHT)) {
+        document.body.classList.replace('light-theme', 'dark-theme');
+        
+        localStorage.setItem('theme', Theme.DARK);
+        return;
+    };
+    
+    document.body.classList.replace('dark-theme', 'light-theme');
+    localStorage.setItem('theme', Theme.LIGHT);
+}
+
+function currentThemeState() {
+    document.body.classList.value = Theme.LIGHT;
+    const userTheme = localStorage.getItem('theme');
+
+    if (userTheme === Theme.DARK) {
+        document.body.classList.replace(Theme.LIGHT, Theme.DARK);
+        themeToggleEl.checked = true;
+    }
+}
